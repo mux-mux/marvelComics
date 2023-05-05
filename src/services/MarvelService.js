5639c14caa741adde2946355c8a617dee6866811
mikemouse <kmmyemail@ukr.net>
2023-05-05 13:57:34 +0300

    feat: add Error Boundaries Component

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
 class MarvelService {
   _apiBase = 'https://gateway.marvel.com:443/v1/public/';
   _apiKey = '01f2e2e123368220041720582fe584a4';
+  _baseOffset = 210;
   getResource = async (url) => {
     const res = await fetch(url);
     //fetch only OK when status 200-299
@@ -10,9 +11,9 @@ class MarvelService {
     return await res.json();
   };
 
-  getAllCharacters = async () => {
+  getAllCharacters = async (offset = this._baseOffset) => {
     const res = await this.getResource(
-      `${this._apiBase}characters?limit=9&offset=210&apikey=${this._apiKey}`
+      `${this._apiBase}characters?limit=9&offset=${offset}&apikey=${this._apiKey}`
     );
     return res.data.results.map(this._transformCharacter);
   };
